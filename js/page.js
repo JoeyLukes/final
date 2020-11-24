9ad4ebadd2fcbc62207bb0380003dc44af5d0328
JoeyLukes <70025189+JoeyLukes@users.noreply.github.com>
2020-11-24 18:39:58 -0500

    Final Commit
    Changed the colors, fixed responsiveness as screen sizes. Added a little "logo" in the nav

--- js/page.js
+++ js/page.js
@@ -1,19 +1,21 @@
-/* const preloader = document.querySelector('.preloader');
+const preloader = document.querySelector('.preloader');
 
 const fadeEffect = setInterval(() => {
   // if we don't set opacity 1 in CSS, then   //it will be equaled to "", that's why we   // check it
   if (!preloader.style.opacity) {
-    preloader.style.opacity = 1;
+    preloader.style.opacity = 2;
   }
   if (preloader.style.opacity > 0) {
-    preloader.style.opacity -= 0.1;
+    preloader.style.opacity -= .15;
   } else {
     clearInterval(fadeEffect);
   }
 }, 200);
-*/
+
 
 window.addEventListener('load', fadeEffect);
+
+setTimeout(function(){ alert("Hello"); }, 3000);
 
 function navToggle() {
   var x = document.getElementById("main-nav");
